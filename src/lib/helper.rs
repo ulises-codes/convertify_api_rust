80e999c53ce004a34af27e872c79472d869f1cfd
Ulises Himely <>
2022-07-03 17:22:39 -0400

    Implement first half of /categories/<category> route and some helper functions

--- src/lib/helper.rs
+++ src/lib/helper.rs
@@ -1,3 +1,37 @@
 pub fn format_key(key: &str) -> String {
     key.to_lowercase().replace(" ", "_")
 }
+
+pub fn format_first_char(word: &str) -> String {
+    let first_letter = word.chars().nth(0).unwrap().to_string().to_uppercase();
+
+    let slice = word[1..].to_string();
+
+    format!("{first_letter}{slice}")
+}
+
+pub fn remove_underscores(key: &str) -> String {
+    print!("{key}");
+
+    key.split("_")
+        .map(|word| format_first_char(word))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn replaces_underscores() {
+        assert_eq!("Square Feet", remove_underscores("square_feet"));
+        assert_eq!("Square Mile", remove_underscores("square_mile"));
+        assert_eq!("Multiply By", remove_underscores("multiply_by"));
+    }
+
+    #[test]
+    fn formats_first_char() {
+        assert_eq!("Sloppy", format_first_char("sloppy"))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,16 @@ mod lib {
     pub mod init_units;
 }
 
-use lib::init_units;
+use std::collections::HashMap;
+
+use lib::{helper::remove_underscores, init_units};
 use rocket::{
     serde::json::{json, Value},
     Build, Rocket,
 };
 
-#[get("/")]
-fn index() -> Value {
+#[get("/categories")]
+fn categories() -> Value {
     let config = init_units::CONFIG.get();
 
     let keys: Vec<&String> = config.units.keys().collect();
@@ -21,9 +23,28 @@ fn index() -> Value {
     json!({ "categories": keys })
 }
 
+#[get("/categories/<category>")]
+fn category(category: &str) -> Value {
+    let config = init_units::CONFIG.get();
+
+    let mut map: HashMap<String, &str> = HashMap::new();
+
+    for (_k, unit_category) in config.units.get(category).unwrap().iter() {
+        if map.contains_key(category) {
+            continue;
+        }
+
+        for (l, to_unit) in unit_category.iter() {
+            map.insert(lib::helper::remove_underscores(&to_unit.from()), l);
+        }
+    }
+
+    json!({ "data": map })
+}
+
 #[launch]
 fn rocket() -> Rocket<Build> {
     init_units::get_data();
 
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![categories, category])
 }
