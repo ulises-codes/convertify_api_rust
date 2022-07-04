654672bc7515abb37f49a16640edf413889f8c98
Ulises Himely <>
2022-07-03 21:01:27 -0400

    Implement multiply feature

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@
 extern crate rocket;
 
 mod lib {
+    pub mod cors;
     pub mod helper;
     pub mod init_units;
 }
 
-use std::collections::HashMap;
-
-use lib::{helper::remove_underscores, init_units};
+use lib::init_units;
 use rocket::{
     serde::json::{json, Value},
     Build, Rocket,
 };
+use std::collections::HashMap;
 
 #[get("/categories")]
 fn categories() -> Value {
@@ -23,11 +23,11 @@ fn categories() -> Value {
     json!({ "categories": keys })
 }
 
-#[get("/categories/<category>")]
-fn category(category: &str) -> Value {
+#[get("/categories/<category>?<from>&<to>&<amount>")]
+fn category(category: &str, from: Option<&str>, to: Option<&str>, amount: Option<f64>) -> Value {
     let config = init_units::CONFIG.get();
 
-    let mut map: HashMap<String, &str> = HashMap::new();
+    let mut map: HashMap<&str, String> = HashMap::new();
 
     for (_k, unit_category) in config.units.get(category).unwrap().iter() {
         if map.contains_key(category) {
@@ -35,16 +35,34 @@ fn category(category: &str) -> Value {
         }
 
         for (l, to_unit) in unit_category.iter() {
-            map.insert(lib::helper::remove_underscores(&to_unit.from()), l);
+            map.insert(l, lib::helper::remove_underscores(&to_unit.to()));
         }
     }
 
-    json!({ "data": map })
+    let from = from.unwrap_or("");
+    let to = to.unwrap_or("");
+    let amount = amount.unwrap_or(0.0);
+
+    if from.len() == 0 || to.len() == 0 || amount <= 0.0 {
+        json!(map)
+    } else {
+        let unit = config.units.get(category).unwrap();
+
+        if map.contains_key(from) && map.contains_key(to) {
+            let x = unit.get(from).unwrap().get(to).unwrap().multiply_by();
+
+            json!({ "result": lib::helper::multiply(amount, x) })
+        } else {
+            json!({"status": 400, "error": "Invalid parameters"})
+        }
+    }
 }
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    init_units::get_data();
+    let _data = init_units::get_data();
 
-    rocket::build().mount("/", routes![categories, category])
+    rocket::build()
+        .mount("/", routes![categories, category])
+        .attach(lib::cors::CORS)
 }
